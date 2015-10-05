51f173dde5edc9c2a6ed2f691634a3cb910ed3d9
Tijmen van Willigen <t.m.vanwilligen@student.utwente.nl>
2015-10-05 12:13:39 +0200

    Update Tijmen.sh

--- Tijmen.sh
+++ Tijmen.sh
@@ -1,1 +1,6 @@
+# This script takes the rating with the year and month
 $ cat reviews_fifty_shades.csv | cut -f 1,2 | sed -E 's/(-[0-9]+)-[0-9]+/\1/'
+
+# Also sorting and counting the uniq 
+$ cat reviews_fifty_shades.csv | cut -f 1,2 | sed -E 's/(-[0-9]+)-[0-9]+/\1/' | sort | uniq -c
+
